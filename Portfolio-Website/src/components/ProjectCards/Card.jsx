56345d8f738070ad24bd99d2cceef532b6aa5c62
aldriechV <aldriechvillamor0922@gmail.com>
2025-07-02 18:02:51 -0700

    Project buttons further designed
    More project buttons implemeneted

--- Portfolio-Website/src/components/ProjectCards/Card.jsx
+++ Portfolio-Website/src/components/ProjectCards/Card.jsx
@@ -1,12 +1,14 @@
-import pookieBear from '../../assets/IMG_4803.jpg'
 import './Card.css'
 
 function Card(){
     return(
         <div className="card">
-            <img className="card-image" src={pookieBear} alt="Project 1"></img>
-            <h2 className="card-title"> Project Name </h2>
-            <p className="card-desc">Hurr durr text here for now</p>
+            <h2 className="card-title"> Real Time Traffic Data Analysis System </h2>
+            <p className="card-desc">A real time Traffic Data Analysis System that allowed users to find and discover critical traffic insights, enhancing decision making in traffic management in real time.</p>
+            <div className ="button-container">
+                <button className="card-button"> More</button>
+                <button className="card-button"> Link</button>
+            </div>
         </div>
     );
 }
