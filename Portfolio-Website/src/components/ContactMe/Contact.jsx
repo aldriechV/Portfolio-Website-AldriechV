71d5bb20f82389c0130ad0db8e2877102753d72c
aldriechV <aldriechvillamor0922@gmail.com>
2025-07-10 18:32:50 -0700

    Implemented container

--- Portfolio-Website/src/components/ContactMe/Contact.jsx
+++ Portfolio-Website/src/components/ContactMe/Contact.jsx
@@ -5,19 +5,37 @@ import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 const Contact = () => {
   return (
     <div className="contactInfo">
-      <h1>Contact Info</h1>
-      <div className='contactItem'>
-        <FaPhone />
-        <span>thi-sis-anum</span>
-      </div>
-      <div className='contactItem'>
-        <FaEnvelope />
-        <span>thisisadomain@gmail.com</span>
-      </div>
-      <div className='contactItem'>
-        <FaMapMarkerAlt />
-        <span>A Place, California</span>
-      </div>
+        <div className='contactLeft'>
+            <h1>Contact Info</h1>
+            <div className='contactItem'>
+                <FaPhone />
+                <span>thi-sis-anum</span>
+            </div>
+            <div className='contactItem'>
+                <FaEnvelope />
+                <span>thisisadomain@gmail.com</span>
+            </div>
+            <div className='contactItem'>
+                <FaMapMarkerAlt />
+                <span>A Place, California</span>
+            </div>
+        </div>
+
+        <div className='contactRight'>
+            <h1>Contact Info</h1>
+            <div className='contactItem'>
+                <FaPhone />
+                <span>thi-sis-anum</span>
+            </div>
+            <div className='contactItem'>
+                <FaEnvelope />
+                <span>thisisadomain@gmail.com</span>
+            </div>
+            <div className='contactItem'>
+                <FaMapMarkerAlt />
+                <span>A Place, California</span>
+            </div>
+        </div>
     </div>
   )
 }
