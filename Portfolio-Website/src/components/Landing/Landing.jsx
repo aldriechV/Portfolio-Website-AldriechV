413a52ea68108c6b0d1cbcb3268517fa0c14799d
aldriechV <aldriechvillamor0922@gmail.com>
2025-08-01 16:46:18 -0700

    cleaned project boxes css

--- Portfolio-Website/src/components/Landing/Landing.jsx
+++ Portfolio-Website/src/components/Landing/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = () => {
         <section>
           <div className='homeContent'>
             <h1>Hi, I'm Aldriech</h1>
-            <p>Full-Stack Developer</p>
-            <p> Here are some more words made to better </p>
+            <h3>Full-Stack Developer</h3>
+            <p> Here are some more words made to better contextualize what text will look like here.</p>
           </div>
           <img src=''></img>
           <div class="buttons">
